ca38ab1f26e7dd1bec210b78829a88bb496014d9
Jean Thomas <tibounise@gmail.com>
2012-10-28 17:00:45 +0100

    We're going to 1.1 !

---
+++ README.md
@@ -0,0 +1,20 @@
+# phpAr
+
+## About phpAr
+phpAr is a simple PHP class to let you unarchive .ar files.
+
+## Usage
+> $archive = new phpAr(/* Name of your file */);
+
+This is how to load a file.
+
+> $archive->listfiles();
+
+This will return an array with all the files inside the archive.
+
+> $archive->getfile(/* File that you want to extract */);
+
+This will return an object with a the informations and the content of the specified file.
+
+## Licence
+phpAr is released under GNU GPL v3 licence
--- phpar.php
+++ phpar.php
@@ -1,65 +1,53 @@
 <?php
-	class phpAr {
-		private $file;
-		private $init;
+// phpAr v1.1
+// Coded by TiBounise (http://tibounise.com)
+// Released as GPL v3 software
 
-		function __construct($file)
-		{
-			if (!empty($file) AND file_exists($file)) {
-				$this->file = $file;
-			}
+class phpAr {
+	private $fileHandler;
+	private $filesize;
+	public function __construct($file) {
+		$this->fileHandler = fopen($file,'rb');
+		if (!$this->fileHandler) {
+			throw new Exception('The archive can\'t be opened');
+		} elseif (!preg_match('#^\!<arch>#',fread($this->fileHandler, 8))) {
+			throw new Exception('Invalid archive file');
 		}
-		function listfiles() {
-			$handle = fopen($this->file,"rb");
-			if (fread($handle, 7) == "!<arch>") {
-				fread($handle, 1);
-				$filesize = filesize($this->file);
-				$list_files = array();
-				while (ftell($handle) < $filesize-1) {
-					$list_files[] = trim(fread($handle, 16));
-					fread($handle, 32);
-					$size = trim(fread($handle, 10));
-					fread($handle, 2);
-					fseek($handle, ftell($handle) + $size);
-				}
-				return $list_files;
-			}
-			else {
-				return false;
-			}
-			fclose($handle);
-		}
-		function getfile($name) {
-			$handle = fopen($this->file,"rb");
-			if (fread($handle, 7) == "!<arch>") {
-				fread($handle, 1);
-				$filesize = filesize($this->file);
-				$file_output = array();
-				while (ftell($handle) < $filesize-1) {
-					$filename = trim(fread($handle, 16));
-					if ($filename == $name) {
-						$timestamp = trim(fread($handle, 12));
-						$owner_id = trim(fread($handle, 6));
-						$group_id = trim(fread($handle, 6));
-						$file_mode = trim(fread($handle, 8));
-						$size = trim(fread($handle, 10));
-						fread($handle, 2);
-						$content = fread($handle, $size);
-						$file_output[] = array($name,$timestamp,$owner_id,$group_id,$file_mode,$size,$content);
-					}
-					else {
-						fread($handle, 32);
-						$size = trim(fread($handle, 10));
-						fread($handle, 2);
-						fseek($handle, ftell($handle)+$size);
-					}
-				}
-				return $file_output;
-			}
-			else {
-				return false;
-			}
-			fclose($handle);
-		} 
+		$this->filesize = filesize($file);
 	}
-?>
+	public function __destruct() {
+		fclose($this->fileHandler);
+	}
+	function listfiles() {
+		$list_files = array();
+		fseek($this->fileHandler,8);
+		while (ftell($this->fileHandler) < $this->filesize - 1) {
+			$list_files[] = trim(fread($this->fileHandler, 16));
+			fread($this->fileHandler, 32);
+			$size = trim(fread($this->fileHandler, 10));
+			fseek($this->fileHandler, 2 + ftell($this->fileHandler) + $size);
+		}
+		return $list_files;
+	}
+	function getfile($name) {
+		$file = new ArrayObject(array(),ArrayObject::STD_PROP_LIST);
+		fseek($this->fileHandler,8);
+		while (ftell($this->fileHandler) < $this->filesize-1) {
+			$filename = trim(fread($this->fileHandler, 16));
+			if ($filename == $name) {
+				$file->timestamp = trim(fread($this->fileHandler, 12));
+				$file->owner_id = trim(fread($this->fileHandler, 6));
+				$file->group_id = trim(fread($this->fileHandler, 6));
+				$file->mode = trim(fread($this->fileHandler, 8));
+				$file->size = trim(fread($this->fileHandler, 10));
+				fread($this->fileHandler, 2);
+				$file->content = fread($this->fileHandler, $file->size);
+			} else {
+				fread($this->fileHandler, 32);
+				$size = trim(fread($this->fileHandler, 10));
+				fseek($this->fileHandler, 2 + ftell($this->fileHandler) + $size);
+			}
+		}
+		return $file;
+	} 
+}
